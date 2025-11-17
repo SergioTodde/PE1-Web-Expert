1443157e079e4e5b9f3b10f2fedbe3ae0b7a9e6d
SergioToddePXL <sergio.todde@student.pxl.be>
2025-11-17 02:35:07 +0100

    aanpassing gedaan in vite.confige.js css kwam niet door

--- Laravel/resources/views/layout.blade.php
+++ Laravel/resources/views/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    @vite(['resources/css/home.css'])
+    @vite(['resources/css/home.css', 'resources/js/app.js'])
     <title>@yield('title')</title>
 </head>
 <body>
--- Laravel/vite.config.js
+++ Laravel/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/home.css',
+                'resources/js/app.js',
             ],
             refresh: true,
         }),
