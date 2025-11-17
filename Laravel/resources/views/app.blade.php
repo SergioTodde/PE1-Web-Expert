60641b30f5cf7f11e28d1f14be4df6928317e492
SergioToddePXL <sergio.todde@student.pxl.be>
2025-11-18 00:03:20 +0100

    aanpassing gedaan vite geconfigureerd

--- Laravel/resources/views/app.blade.php
+++ Laravel/resources/views/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Event App</title>
-    @vite('resources/js/app.js')
+    @vite(['resources/js/app.js', 'resources/css/app.css']);
 </head>
 <body>
 <div id="app"></div>
