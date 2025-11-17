316c73680bd35b72ed176ad676561189e90a001d
SergioToddePXL <sergio.todde@student.pxl.be>
2025-11-17 02:50:35 +0100

    aanpassing gedaan app.blade en route toegevoegd en testbestanden weggedaan

---
+++ Laravel/resources/views/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Event App</title>
+    @vite('resources/js/app.js')
+</head>
+<body>
+<div id="app"></div>
+</body>
+</html>
